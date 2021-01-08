14521b080af0e831b3c70f32d7cb4afc4c0869bd
bikke9 <bike.ggg.b9k5@gmail.com>
2021-01-08 23:17:50 +0900

    Initial Commit

---
+++ TabviewSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabviewSample
+//
+//  Created by 飯島啓之 on 2021/01/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabviewSample/TabviewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabviewSampleApp.swift
+//  TabviewSample
+//
+//  Created by 飯島啓之 on 2021/01/08.
+//
+
+import SwiftUI
+
+@main
+struct TabviewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
